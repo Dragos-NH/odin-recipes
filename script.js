eb9dd0e36fa61bc27d60f503e0047a18199c6a4e
Dragos-NH <edragos32@gmail.com>
2025-01-03 11:56:52 +0100

    Ceated cutomer review
    I added a customer review inside Index.html and made it as a caroussel that can be changed using buttons but with a timer as well

--- script.js
+++ script.js
@@ -1,11 +1,36 @@
 let switchImg = document.querySelectorAll(".caroussel-img");
-let img = 0;
+let activeProfile = document.querySelectorAll(".profile");
+let rightArr = document.querySelector(".rightArr");
+let leftArr = document.querySelector(".leftArr");
+let currentImg = 0;
+let currentIndex = 0;
 
 const caroussel = () => {
-    switchImg[img].classList.remove("visible");
-    img = (img + 1) % switchImg.length;
-    switchImg[img].classList.add("visible");
+    switchImg[currentImg].classList.remove("visible");
+    currentImg = (currentImg + 1) % switchImg.length;
+    switchImg[currentImg].classList.add("visible");
 
 }
 
 setInterval(caroussel, 2500);
+
+
+rightArr.addEventListener("click", function(){
+    activeProfile[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % activeProfile.length;
+    activeProfile[currentIndex].classList.add("active");
+});
+
+leftArr.addEventListener("click", function(){
+    activeProfile[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex - 1 + activeProfile.length ) % activeProfile.length;
+    activeProfile[currentIndex].classList.add("active");
+})
+
+const profileChange = () => {
+    activeProfile[currentIndex].classList.remove("active");
+    currentIndex = (currentIndex + 1) % activeProfile.length;
+    activeProfile[currentIndex].classList.add("active");
+}
+
+setInterval(profileChange, 3000);
